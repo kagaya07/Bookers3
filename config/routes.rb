300dd7c9416ac128f4f8fad00a7ad14735727e9d
vagrant <vagrant@localhost.localdomain>
2020-06-06 11:17:59 +0000

    [add]5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
   devise_for :users
+  root :to => 'users#top'
+  get 'books' => 'books#index'
+  post 'books', to: 'books#create'
+  get 'books/:id' => 'books#show', as:'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
